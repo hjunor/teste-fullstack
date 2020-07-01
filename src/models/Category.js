54368611c8469568b68c1e988b2085c13698edc1
hjunor <hjunor@gmail.com>
2020-06-30 22:28:14 -0300

    Category schema

---
+++ src/models/Category.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const category = new schema(
+  {
+    titulo: {
+      trim: true,
+      createIndexes: true,
+      required: true,
+      type: String,
+    },
+    descricao: {
+      type: String,
+      required: true,
+    },
+    dataCriacao: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false }
+);
+
+categoria_model.pre('save', (next) => {
+  let agora = new Date();
+  if (!this.dataCriacao) {
+    this.dataCriacao = agora;
+    next();
+  }
+});
+
+module.exports = mongoose.model('Category', category);
