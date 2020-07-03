c6c92ea39df1d2c191877e0235142ec3d3f53528
hjunor <hjunor@gmail.com>
2020-07-02 21:24:38 -0300

    mongoDb config

--- src/services/mongoServices.js
+++ src/services/mongoServices.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const URL = `mongodb://dev:3n5df3k9@localhost:27017/dev`;
 
 const option = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 };
 
 mongoose.connect(URL, option, (erro) => {
   !erro ? console.log('connect') : console.log(erro);
 });
+
+module.exports = mongoose;
