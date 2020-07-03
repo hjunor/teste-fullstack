e4c7fb30f387e4817f971731a9b582d6fce68162
hjunor <hjunor@gmail.com>
2020-07-02 21:25:20 -0300

    server body-parser

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 require('./services/mongoServices');
+const bodyParser = require('body-parser');
 const express = require('express');
 const routes = require('./router');
 
 const app = express();
-
+// app.use(bodyParser.urlencoded());
 app.use(express.json());
-// app.use(express.urlencoded());
 
 app.use(routes);
 
