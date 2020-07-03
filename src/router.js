e49feda2fd0ba95a1f5ee4931d13771066e8cb50
hjunor <hjunor@gmail.com>
2020-07-02 21:24:58 -0300

    routes auth

--- src/router.js
+++ src/router.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/', (require, response) => {
-  response.json({ mesage: 'ok' });
-});
+const auth = require('./middleware/authMiddleware');
+
+const UsersController = require('./controllers/UsersController');
+const AuthController = require('./controllers/AuthController');
+
+routes.post('/user', UsersController.create);
+routes.post('/auth', AuthController.authenticate);
+routes.get('/user', auth, UsersController.store);
 
 module.exports = routes;
