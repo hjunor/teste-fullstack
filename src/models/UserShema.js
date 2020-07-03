1e9589f57a35ed3382c663d91dc32a0f48808b84
hjunor <hjunor@gmail.com>
2020-07-02 21:24:05 -0300

    update shema of users

--- src/models/UserShema.js
+++ src/models/UserShema.js
@@ -2,28 +2,33 @@ const mongoose = require('mongoose');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 
-const userShema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: uuid.v4(),
+const userShema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: uuid.v4(),
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/],
+    },
+    password: {
+      type: String,
+      required: true,
+      select: true,
+    },
   },
-  name: {
-    required: true,
-    type: String,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    match: [/\S+@\S+\.\S+/],
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userShema.pre('save', async function (next) {
   const hash = await bcrypt.hash(this.password, 10);
