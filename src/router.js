5dc0233a36d441f0fb9894ee378d5a332deb82e4
hjunor <hjunor@gmail.com>
2020-07-03 11:30:42 -0300

    routes category

--- src/router.js
+++ src/router.js
@@ -6,9 +6,13 @@ const auth = require('./middleware/authMiddleware');
 
 const UsersController = require('./controllers/UsersController');
 const AuthController = require('./controllers/AuthController');
+const CategoryController = require('./controllers/CategoryController');
+
+routes.post('/auth', AuthController.authenticate);
 
 routes.post('/user', UsersController.create);
-routes.post('/auth', AuthController.authenticate);
 routes.get('/user', auth, UsersController.store);
+
+routes.post('/category', auth, CategoryController.create);
 
 module.exports = routes;
