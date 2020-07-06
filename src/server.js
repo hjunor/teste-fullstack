57086d0ff55b71f1b28c0dccb3892c67a9a20e59
hjunor <hjunor@gmail.com>
2020-07-06 09:16:48 -0300

    server edit

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const express = require('express');
 const routes = require('./router');
 
 const app = express();
-// app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded());
 app.use(express.json());
-//Ok
+
 app.use(routes);
 
 app.listen(3333);
