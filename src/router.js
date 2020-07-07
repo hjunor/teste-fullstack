a15a0755e895aa5b624e4b5c86ba9b8fad24b0fb
hjunor <hjunor@gmail.com>
2020-07-06 22:05:03 -0300

    Add routes of Category

--- src/router.js
+++ src/router.js
@@ -9,11 +9,13 @@ const AuthController = require('./controllers/AuthController');
 const CategoryController = require('./controllers/CategoryController');
 
 routes.post('/auth', AuthController.authenticate);
-
 routes.post('/user', UsersController.create);
 routes.get('/user', auth, UsersController.store);
 
 routes.post('/category', auth, CategoryController.create);
-routes.get('/category', auth, CategoryController.store);
+routes.get('/category', auth, CategoryController.storeAll);
+routes.get('/category/:id', auth, CategoryController.store);
+routes.post('/category/update/:id', auth, CategoryController.update);
+routes.get('/category/delete/:id', auth, CategoryController.delete);
 
 module.exports = routes;
