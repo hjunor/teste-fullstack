95aebfba521377ee956b60badfd650b8bfcdd997
hjunor <hjunor@gmail.com>
2020-07-08 00:32:36 -0300

    config add BrendShema.js

--- src/models/BrendShema.js
+++ src/models/BrendShema.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 const uuid = require('uuid');
+
+const BrendShema = new mongoose.Schema({
+
+  _id: {
+    type: String,
+    default: true
+  },
+  name: {
+    type: String,
+    require: true
+
+  }
+
+})
